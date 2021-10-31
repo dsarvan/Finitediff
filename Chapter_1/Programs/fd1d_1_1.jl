9cdc62167d40492e4901cf8d95463ef960f5580e
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-10-31 17:46:42 +0530

    simulation in free space

--- Chapter_1/Programs/fd1d_1_1.jl
+++ Chapter_1/Programs/fd1d_1_1.jl
@@ -7,38 +7,42 @@
 
 import PyPlot
 const plt = PyPlot
+plt.rc("pgf", texsystem="pdflatex")
+plt.rc("font", family="serif", weight="normal", size=8)
+plt.rc("axes", labelsize=10, titlesize=10)
+plt.rc("figure", titlesize=10)
+plt.rc("text", usetex="True")
 
 ke = 200
 ex = zeros(ke)
 hy = zeros(ke)
 
 # Pulse parameters
-kc = Int(ke/2)
+kc = round(Int64, ke/2)
 t0 = 40
 spread = 12
 nsteps = 100
 
 # FDTD loop
-for time_step in 2:nsteps
+for time_step in 1:nsteps
     
     # calculate the Ex field
     for k in 2:ke
         ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k])
-        println(ex[k])
     end
 
     # put a Gaussian pulse in the middle
     ex[kc] = exp(-0.5 * ((t0 - time_step)/spread)^2)
     
     # calculate the Hy field
-    for k in 2:(ke - 1)
+    for k in 1:(ke - 2)
         hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
     end
 
 end
 
 #fig, (ax1, ax2) = plt.subplots(2)
-##fig.subtitle(raw"FDTD simulation of a pulse in free space after 100 time steps")
+#fig.subtitle(raw"FDTD simulation of a pulse in free space after 100 time steps")
 #ax1.plot(ex, 'k', lw=1)
 #ax2.plot(hy, 'k', lw=1)
 #plt.subplots_adjust(bottom=0.2, hspace=0.45)
