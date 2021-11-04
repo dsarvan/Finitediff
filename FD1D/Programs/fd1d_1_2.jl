676b0eea808ce82669485347ab0f458a3926054c
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-11-04 13:10:57 +0530

    simulation of a pulse with absorbing boundary conditions

--- FD1D/Programs/fd1d_1_2.jl
+++ FD1D/Programs/fd1d_1_2.jl
@@ -13,7 +13,7 @@ plt.rc("axes", labelsize = 10, titlesize = 10)
 plt.rc("figure", titlesize = 10)
 using LaTeXStrings
 
-ke = 201
+ke = 202
 ex = zeros(ke)
 hy = zeros(ke)
 
@@ -21,16 +21,16 @@ hy = zeros(ke)
 kc = round(Int64, ke / 2)
 t0 = 40
 spread = 12
-nsteps = 100
+nsteps = 250
 
-boundary_low = [0., 0.]
-boundary_high = [0., 0.]
+boundary_low = [0.0, 0.0]
+boundary_high = [0.0, 0.0]
 
 # desired points for plotting
 points = [
-          Dict("num_steps" => 100, "data" => nothing, "label" => " "),
-          Dict("num_steps" => 225, "data" => nothing, "label" => " "),
-          Dict("num_steps" => 250, "data" => nothing, "label" => "FDTD cells")
+    Dict("num_steps" => 100, "data" => nothing, "label" => " "),
+    Dict("num_steps" => 225, "data" => nothing, "label" => " "),
+    Dict("num_steps" => 250, "data" => nothing, "label" => "FDTD cells"),
 ]
 
 # FDTD loop
@@ -47,11 +47,11 @@ for time_step = 1:nsteps
     # absorbing boundary conditions
     ex[1] = popfirst!(boundary_low)
     push!(boundary_low, ex[2])
-    ex[ke] = popfirst!(boundary_high)
-    push!(boundary_high, ex[ke - 1])
+    ex[ke-1] = popfirst!(boundary_high)
+    push!(boundary_high, ex[ke-2])
 
     # calculate the Hy field
-    for k = 1:(ke-2)
+    for k = 1:(ke-1)
         hy[k] = hy[k] + 0.5 * (ex[k] - ex[k+1])
     end
 
@@ -64,22 +64,22 @@ for time_step = 1:nsteps
 
 end
 
-fig = plt.figure(figsize=(8, 5.25))
+fig = plt.figure(figsize = (8, 5.25))
 fig.suptitle(raw"FDTD simulation with absorbing boundary conditions")
 
 
-function plotting(data, timestep, label)
+function plotting(ax, data, timestep, label)
     """plot of E field at a single time step"""
-    ax.plot(data, color="k", linewidth=1)
-    ax.set(xlim=(0, 200), ylim=(-0.2, 1.2), xlabel="label", ylabel=L"E$_x$")
-    ax.set(xticks=0:20:200, yticks=0:1:1.2)
-    ax.text(100, 0.5, "T = time", horizontalalignment="center")
+    ax.plot(data, "k", linewidth = 1)
+    ax.set(xlim = (0, 200), ylim = (-0.2, 1.2), xlabel = "$label", ylabel = L"E$_x$")
+    ax.set(xticks = 0:20:200, yticks = 0:1:1.2)
+    ax.text(100, 0.5, "T = $timestep", horizontalalignment = "center")
 end
 
 
-for subplot_num, plot_data in enumerate(points)
-    ax = fig.add_subplot(3, 1, subplot_num + 1)
-    plotting(plot_data["data"], plot_data["num_steps"], plot_data["label"])
+for (subplot_num, plot_data) in enumerate(points)
+    ax = fig.add_subplot(3, 1, subplot_num)
+    plotting(ax, plot_data["data"], plot_data["num_steps"], plot_data["label"])
 end
 
 plt.tight_layout()
